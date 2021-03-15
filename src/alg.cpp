7a4b30f1d6835a59fa8cc39b14391b1ca404bb6a
telinak <78906896+telinak@users.noreply.github.com>
2021-03-15 16:09:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,3 +1,2 @@
 // Copyright 2021 NNTU-CS
-int gcd(int a, int b) {  if {(b == 0)  return a;}  else  {return gcd (b, a % b);}
-}
+int gcd(int a, int b){  while(b) {  a%=b;  swap(a, b);  }  return a;}

2beb27c5d2ac224d88d5bf5f6eaa46b2501cdf11
telinak <78906896+telinak@users.noreply.github.com>
2021-03-15 16:02:25 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,3 +1,3 @@
 // Copyright 2021 NNTU-CS
-int gcd(int a, int b) {  if (b == 0)  return a;  else  return gcd (b, a % b);
+int gcd(int a, int b) {  if {(b == 0)  return a;}  else  {return gcd (b, a % b);}
 }
